ad5467a739038f133921e037df77ae1d35534f6a
Nicolas <nico@neurona.uy>
2021-06-11 17:52:47 -0300

    working on FormTarea,

--- src/components/tareas/FormTarea.js
+++ src/components/tareas/FormTarea.js
@@ -1,28 +1,37 @@
 /** @format */
 
-import React from "react";
+import React, { Fragment, useContext } from "react";
+import proyectoContext from "../../context/proyectos/proyectoContext";
 
 const FormTarea = () => {
+  // state de proyectos
+  const contextProyecto = useContext(proyectoContext);
+  const { proyecto } = contextProyecto;
+
   return (
-    <div className="formulario">
-      <form action="">
-        <div className="contenedor-input">
-          <input
-            type="text"
-            className="input-text"
-            placeholder="Nombre de la tarea..."
-            name="nombre"
-          />
+    <Fragment>
+      {proyecto ? (
+        <div className="formulario">
+          <form action="">
+            <div className="contenedor-input">
+              <input
+                type="text"
+                className="input-text"
+                placeholder="Nombre de la tarea..."
+                name="nombre"
+              />
+            </div>
+            <div className="contenedor-input">
+              <input
+                type="submit"
+                value="Agregar Tarea"
+                className="btn btn-primario btn-block"
+              />
+            </div>
+          </form>
         </div>
-        <div className="contenedor-input">
-          <input
-            type="submit"
-            value="Agregar Tarea"
-            className="btn btn-primario btn-block"
-          />
-        </div>
-      </form>
-    </div>
+      ) : null}
+    </Fragment>
   );
 };
 
